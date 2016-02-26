53e46ddce639bf3c4f8f5c2664ad2d91a3923fc6
Henry Roe <hroe@hroe.me>
2016-02-25 18:12:15 -0700

    changed index page creation script to make text more readable on small devices

--- create-index-page.sh
+++ create-index-page.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls schedules/schedule*html | awk '{print "<a href=\""$1"\">"$1"</a><br>\n"}' | sed -e 's/.html//2' | sed -e 's/schedules\/schedule-//2' > index.html
+ls schedules/schedule*html | awk '{print "<h1><a href=\""$1"\">"$1"</a></h1><br>\n"}' | sed -e 's/.html//2' | sed -e 's/schedules\/schedule-//2' > index.html
